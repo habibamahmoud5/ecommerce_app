ff86df9ae27e97db12be7d507ac3d65ad935b642
habibamahmoud-design <enghabibamahmoud@gmail.com>
2026-09-07 22:33:43 +0300

    add apptextstyle& colors style

---
+++ lib/core/app_text_style.dart
@@ -0,0 +1,73 @@
+import 'package:flutter/material.dart';
+
+class AppTextStyles {
+  static const TextStyle h1SemiBold = TextStyle(
+    fontSize: 64,
+    fontWeight: FontWeight.w600,
+  );
+
+  static const TextStyle h2SemiBold = TextStyle(
+    fontSize: 32,
+    fontWeight: FontWeight.w600,
+  );
+
+  static const TextStyle h3SemiBold = TextStyle(
+    fontSize: 24,
+    fontWeight: FontWeight.w600,
+  );
+
+  static const TextStyle h4SemiBold = TextStyle(
+    fontSize: 20,
+    fontWeight: FontWeight.w600,
+  );
+
+  static const TextStyle h4Medium = TextStyle(
+    fontSize: 20,
+    fontWeight: FontWeight.w500,
+  );
+
+  static const TextStyle b1Regular = TextStyle(
+    fontSize: 16,
+    fontWeight: FontWeight.w400,
+  );
+
+  static const TextStyle b1Medium = TextStyle(
+    fontSize: 16,
+    fontWeight: FontWeight.w500,
+  );
+
+  static const TextStyle b1SemiBold = TextStyle(
+    fontSize: 16,
+    fontWeight: FontWeight.w600,
+  );
+
+  static const TextStyle b2Regular = TextStyle(
+    fontSize: 14,
+    fontWeight: FontWeight.w400,
+  );
+
+  static const TextStyle b2Medium = TextStyle(
+    fontSize: 14,
+    fontWeight: FontWeight.w500,
+  );
+
+  static const TextStyle b2SemiBold = TextStyle(
+    fontSize: 14,
+    fontWeight: FontWeight.w600,
+  );
+
+  static const TextStyle b3Regular = TextStyle(
+    fontSize: 12,
+    fontWeight: FontWeight.w400,
+  );
+
+  static const TextStyle b3Medium = TextStyle(
+    fontSize: 12,
+    fontWeight: FontWeight.w500,
+  );
+
+  static const TextStyle b3SemiBold = TextStyle(
+    fontSize: 12,
+    fontWeight: FontWeight.w600,
+  );
+}
---
+++ lib/core/colors.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static const Color black = Color(0xff1A1A1A);
+  static const Color darkGray = Color(0xff333333);
+  static const Color gray = Color(0xff4D4D4D);
+  static const Color gray666 = Color(0xff666666);
+  static const Color gray808 = Color(0xff808080);
+  static const Color gray999 = Color(0xff999999);
+  static const Color grayB3 = Color(0xffB3B3B3);
+  static const Color grayCC = Color(0xffCCCCCC);
+  static const Color lightGray = Color(0xffE6E6E6);
+  static const Color white = Color(0xffFFFFFF);
+  static const Color green = Color(0xff0C9409);
+  static const Color red = Color(0xffED1010);
+}
