6e738c2c41b0008c20fa54601e90dda13d0e2da0
habibamahmoud-design <enghabibamahmoud@gmail.com>
2026-09-07 11:34:07 +0300

    Add project folder structure

---
+++ lib/ecommerce_app.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class EcommerceApp extends StatelessWidget {
+  const EcommerceApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(debugShowCheckedModeBanner: false);
+  }
+}
